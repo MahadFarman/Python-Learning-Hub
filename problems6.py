abaea9584ee65ba624b5bfc4f71a878912c64084
MahadFarman <mahadfarman86@gmail.com>
2026-09-01 17:00:19 +0500

    ch_6-prac

---
+++ problems6.py
@@ -0,0 +1,67 @@
+# # 1. Write a program to find the greatest of 4 numbers entered by the user using if case.
+# a1=int(input("Enter the number 1 :"))
+# a2=int(input("Enter the number 2 :"))
+# a3=int(input("Enter the number 3 :"))
+# a4=int(input("Enter the number 4 :"))
+
+# if(a1>a2 and a1> a3 and a1>a4):
+#     print(f'It is the greatest in these numbers : {a1}')
+
+# elif(a2>a1 and a2>a3 and a2> a4):
+#     print(f'It is the greatest in these numbers : {a2}')
+
+# elif(a3>a1 and a3>a2 and a3>a4):
+#     print(f'It is the greatest in these numbers : {a3}')
+
+# else:
+#     print(f'It is the greatest in these numbers : {a4}')
+
+# 2. Write a program to find out whether a student has passed or failed if it requires a total of
+# 40% and at least 33% in each subject to pass. Assume 3 subjects and take marks as an
+# input from the user.
+
+# marks1=int(input('Enter the marks 1 :'))
+# marks2=int(input('Enter the marks 2 :'))
+# marks3=int(input('Enter the marks 3 :'))
+
+# total_percentage=(100*(marks1+marks2+marks3)/300)
+
+# if(total_percentage>40 or (marks1>34 and marks2>34 and marks3>34)):
+#     print('Congraculations ! You are passed',total_percentage)
+
+# else:
+#     print('Bad luck! You are failed',total_percentage)
+
+#3.A spam comment is defined as a text containing following keywords: “Make a lot of
+# money”, “buy now”, “subscribe this”, “click this”. Write a program to detect these spams.
+# s1='Make a lot of money'
+# s2='buy now'
+# s3='subscribe this'
+# s4='click this'
+
+# comment=input('Enter your comment :')
+
+# if(s1 in comment or s2 in comment or s3 in comment or s4 in comment):
+#     print('Its a spam')
+
+# else:
+#     print('Its not a spam')
+
+# 4. Write a program to find whether a given username contains less than 10 characters or not.
+
+
+# 5. Write a program which finds out whether a given name is present in a list or not.
+# scheme:
+# 90 – 100 => Ex
+# 80 – 90 => A
+# 70 – 80 => B
+# 60 – 70 => C
+# 50 – 60 => D
+# <50 => F
+
+
+# 6. Write a program to calculate the grade of a student from his marks from the following
+
+
+# 7. Write a program to find out whether a given post is talking about “Harry” or not.
+
