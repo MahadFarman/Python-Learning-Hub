4da9fa8a14c7070f14fe8d7adcc1b9568e9c4278
MahadFarman <mahadfarman86@gmail.com>
2026-09-01 16:57:03 +0500

    ch_4-prac

---
+++ problems4.py
@@ -0,0 +1,64 @@
+# 1. Write a program to store seven fruits in a list entered by the user.
+fruits=[]
+
+f1=(input('Enter the furit name :'))
+fruits.append(f1)
+
+f2=(input('Enter the furit name :'))
+fruits.append(f2)
+
+f3=(input('Enter the furit name :'))
+fruits.append(f3)
+
+f4=(input('Enter the furit name :'))
+fruits.append(f4)
+
+f5=(input('Enter the furit name :'))
+fruits.append(f5)
+
+f6=(input('Enter the furit name :'))
+fruits.append(f6)
+
+f7=(input('Enter the furit name :'))
+fruits.append(f7)
+
+print(fruits)
+
+# 2. Write a program to accept marks of 6 students and display them in a sorted manner.
+marks=[]
+
+m1=(input('Enter the marks name :'))
+marks.append(m1)
+
+m2=(input('Enter the marks name :'))
+marks.append(m2)
+
+m3=(input('Enter the marks name :'))
+marks.append(m3)
+
+m4=(input('Enter the marks name :'))
+marks.append(m4)
+
+m5=(input('Enter the marks name :'))
+marks.append(m5)
+
+m6=(input('Enter the marks name :'))
+marks.append(m6)
+
+m7=(input('Enter the marks name :'))
+marks.append(m7)
+
+print(marks)
+
+# 3. Check that a tuple type cannot be changed in python.
+a=(1,3,4,'mahad')
+a[2]='aaryy'
+tuple cannot be changed
+
+# 4. Write a program to sum a list with 4 numbers.
+a = (7, 0, 8, 0, 0, 9)
+print(sum(a))
+# 5. Write a program to count the number of zeros in the following tuple
+a = (7, 0, 8, 0, 0, 9)
+b=a.count(0)
+print(b)
