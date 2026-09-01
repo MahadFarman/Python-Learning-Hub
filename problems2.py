0e138b525b9b2362bd7d4d71c812afbcbd54a947
MahadFarman <mahadfarman86@gmail.com>
2026-09-01 16:52:56 +0500

    ch_2-prac

---
+++ problems2.py
@@ -0,0 +1,27 @@
+# 1. Write a python program to add two numbers.
+n1=int(input('Enter the number 1 :'))
+n2=int(input('Enter the number 2 :'))
+print(n1+n2)
+
+# 2. Write a python program to find remainder when a number is divided by z.
+a=int(input('Enter the number :'))
+b=int(input('Enter the number :'))
+print('Remainder when a divied by b :',a%b)
+
+# 3. Check the type of variable assigned using input() function.
+a=input('enter the variable :')
+print(type(a))
+
+# 4. Use comparison operator to find out whether ‘aʼ input variable is greater than ‘bʼ or not.
+a=int(input('Enter the number :'))
+b=int(input('Enter the number :'))
+print('a is greter than b ',a>b)
+
+# 5. Write a python program to find an average of two numbers entered by the user.
+a=int(input('Enter the number :'))
+b=int(input('Enter the number :'))
+print('The average of two these two number is:', (a+b)/2)
+
+# 6. Write a python program to calculate the square of a number entered by the user
+a=int(input('Enter the number :'))
+print("The square of this number is :", a*a)
